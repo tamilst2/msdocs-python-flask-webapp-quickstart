91eebc396a71d4e1033fcddf269d9547243d7399
Tamil <tamil.selvam@avanade.com>
2023-09-18 22:56:04 +1000

    added screen awake

--- app.py
+++ app.py
@@ -1,32 +1,40 @@
 import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, render_template
+import pyautogui
+import threading
 
 app = Flask(__name__)
 
+# Set the interval (in seconds) at which to move the mouse cursor
+interval_seconds = 5  # Change this as needed
+
+# Flag to indicate whether to keep the screen awake
+keep_screen_awake = False
+
+def keep_awake():
+    global keep_screen_awake
+    while keep_screen_awake:
+        pyautogui.moveRel(1, 1)
+        pyautogui.moveRel(-1, -1)
+        pyautogui.sleep(interval_seconds)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/start')
+def start():
+    global keep_screen_awake
+    if not keep_screen_awake:
+        keep_screen_awake = True
+        threading.Thread(target=keep_awake).start()
+    return "Screen awake mode is enabled."
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+@app.route('/stop')
+def stop():
+    global keep_screen_awake
+    keep_screen_awake = False
+    return "Screen awake mode is disabled."
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
